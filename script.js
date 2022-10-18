e73dc0ef7b7c46de28ccd2a55f3e48b0ddcdcf7a
Santosh Bhandari <santoshvandari1@gmail.com>
2022-10-18 13:58:08 +0545

    Personal Website

---
+++ script.js
@@ -0,0 +1,7 @@
+// Auto typing text 
+var typing = new Typed("#type",{
+    strings:["Front-End Developer","Video Editor","Content Writer","Graphic Designer","Learner"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
