bbbaf573b24febe2538ebbd03fe09ce75563598c
santoshvandari <santoshvandari1@gmail.com>
2023-07-30 21:30:55 +0545

    Updated

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ document.addEventListener("DOMContentLoaded",function(){
       loop: true
   })
 })
+//image Changing Animation
+let img = document.querySelector(".img");
+setInterval(() => {
+  if(img.classList.contains("img1")){
+    img.classList.remove("img1");
+  }else{
+    img.classList.add("img1");
+  }
+}, 5000);
