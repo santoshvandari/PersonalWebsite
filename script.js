732bb1140d9466d0e8b1474db315aaef1dddb907
santoshvandari <santoshvandari1@gmail.com>
2023-08-03 21:51:00 +0545

    Inital Commit

--- script.js
+++ script.js
@@ -7,20 +7,4 @@ document.addEventListener("DOMContentLoaded",function(){
       backSpeed: 120,
       loop: true
   })
-  //image Changing Animation
-  let img = document.querySelector(".img");
-  let i=2;
-  setInterval(() => {
-    if(i==13)
-      i=1;
-    let styledata=`background: url('/img/img${i}.jpg') center center/cover;`;
-    if(i%2){
-      styledata+="transform: rotatey(360deg);"
-    }else{
-    styledata+="transform: rotatey(720deg);"
-    }
-    console.log(styledata);
-    img.style=styledata;
-    i++;
-  }, 5000);
 })
