686f8e8ee200b5b9175332a3d5d5f5659575a979
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-14 17:41:02 +0545

    Initial

--- script.js
+++ script.js
@@ -11,4 +11,16 @@
 
 
 
-
+function QuotesFetch(){
+    URL='https://api.quotable.io/random';
+  /* margin: 10px 0; */
+    URL='https://zenquotes.io/api/quotes';
+    fetch(url)
+    .then(response=>response.json())
+    .then(response=>{
+        console.log(response);
+    })
+    .catch(error=>{
+        console.log(error);
+    })
+}
