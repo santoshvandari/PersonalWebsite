2a668ba899c6833b593330093d8ac794a801a560
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-27 07:43:10 +0545

    Preloader added

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 "use strict";
-document.addEventListener("DOMContentLoaded",function(){
-    function QuotesFetch(){
-        let url='https://quotesapi.santosh0.com.np/api/quotes/';
-        fetch(url)
-        .then(response=>response.json())
-        .then(response=>{
-            document.getElementById("quotes").textContent =response[0].quotes;
-            document.getElementById("author").textContent = "- "+response[0].author;
-            setTimeout(QuotesFetch,10000);
-        })
-        .catch(error=>{
-            console.log(error);
-        })
-    }
-    QuotesFetch()
-})
+(()=>{
+    document.addEventListener("DOMContentLoaded",function(){
+        function QuotesFetch(){
+            let url='https://quotesapi.santosh0.com.np/api/quotes/';
+            fetch(url)
+            .then(response=>response.json())
+            .then(response=>{
+                document.getElementById("quotes").textContent =response[0].quotes;
+                document.getElementById("author").textContent = "- "+response[0].author;
+                setTimeout(QuotesFetch,10000);
+            })
+            .catch(error=>{
+                console.log(error);
+            })
+        }
+        QuotesFetch()
+    })
+})();
+
 
