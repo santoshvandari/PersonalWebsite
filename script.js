0211e7da315c5119114cec93b18c6aaa67f5985d
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-27 07:45:01 +0545

    Preloader added

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 "use strict";
-(()=>{
-    document.addEventListener("DOMContentLoaded",function(){
-        function QuotesFetch(){
-            let url='https://quotesapi.santosh0.com.np/api/quotes/';
+(() => {
+        function QuotesFetch() {
+            let url = 'https://quotesapi.santosh0.com.np/api/quotes/';
             fetch(url)
-            .then(response=>response.json())
-            .then(response=>{
-                document.getElementById("quotes").textContent =response[0].quotes;
-                document.getElementById("author").textContent = "- "+response[0].author;
-                setTimeout(QuotesFetch,10000);
-            })
-            .catch(error=>{
-                console.log(error);
-            })
+                .then(response => response.json())
+                .then(response => {
+                    document.getElementById("quotes").textContent = response[0].quotes;
+                    document.getElementById("author").textContent = "- " + response[0].author;
+                    setTimeout(QuotesFetch, 10000);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
         }
         QuotesFetch()
-    })
+
 })();
 
 
